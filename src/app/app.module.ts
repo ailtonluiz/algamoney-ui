16d1cec06d9ffae102d195b1f9f8231bd77356c8
Ailton Luiz <ailtonatn@gmail.com>
2021-03-29 12:06:43 +0200

    Prototipando a tela de lancamento parte 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
-import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +12,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    InputTextModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
